fb6f232122d4e6beadd2ab41a4fc067d40b28c5b
Thiago Santos <thia.mdossantos@gmail.com>
2018-03-06 21:01:07 -0300

    ci(travis): add travis config file

--- test/safe-money.test.js
+++ test/safe-money.test.js
@@ -2,15 +2,15 @@ import * as safeMoney from '../lib/safe-money'
 
 describe('safeMoney', () => {
   describe('fromInteger', () => {
-    it('should return money from integer', function() {
+    it('should return money from integer', () => {
       const money = safeMoney.fromInteger(1000, safeMoney.EUR)
 
       expect(money.amount).toEqual(1000)
       expect(money.currency).toEqual('EUR')
     })
 
-    it('should not create a new instance from decimal', function() {
-      expect(function() {
+    it('should not create a new instance from decimal', () => {
+      expect(() => {
         safeMoney.fromInteger(10.42, safeMoney.EUR)
       }).toThrow(TypeError)
     })
